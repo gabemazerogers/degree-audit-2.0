6f190b8aaa4d3e9e19bd7f067a66d5801c526c71
root <root@degree-audit-2.0>
2017-01-14 23:18:20 +0000

    Updated API to allow CORS on specified domains

--- api/app.py
+++ api/app.py
@@ -3,11 +3,22 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.join(os.getcwd(), os.par
 import main
 
 import falcon
+from falcon_cors import CORS
 import routes
+import requests
+
+ALLOWED_ORIGINS = ['http://localhost:3000', 'http://degree-audit-2.herokuapp.com']
+
+class CorsMiddleware(object):
+
+    def process_request(self, request, response):
+        origin = request.get_header('Origin')
+        if origin in ALLOWED_ORIGINS:
+            response.set_header('Access-Control-Allow-Origin', origin)
 
 main.init()
 
-api = application = falcon.API()
+api = application = falcon.API(middleware=[CorsMiddleware()])
 
 auth = routes.Auth()
 courses = routes.Courses()
--- api/routes.py
+++ api/routes.py
@@ -3,7 +3,6 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.join(os.getcwd(), os.par
 import main
 import urllib
 import falcon
-import msgpack
 import uuid
 import mimetypes
 import redis
