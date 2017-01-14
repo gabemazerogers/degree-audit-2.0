a2f1d04ec240c9fade0c8f01068c290d59e4e7ff
Gabe Maze-Rogers <gmazerogers@gmail.com>
2017-01-13 22:05:57 -0800

    Fixing heroku port error

--- server.js
+++ server.js
@@ -5,6 +5,4 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Express server is up on port 3000');
-});
+app.listen(process.env.PORT || 3000);
